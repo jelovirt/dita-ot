49c28f7802ee69f3430c9988691cffbfd20c1d04
Jarno Elovirta <jarno@elovirta.com>
2016-08-08 07:43:12 +0300

    Add initial code for JavaFX based example GUI

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,133 @@
+package sample;
+
+import javafx.collections.ObservableList;
+import javafx.css.PseudoClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.*;
+
+import static javafx.collections.FXCollections.observableList;
+
+public class Controller implements Initializable {
+
+    private final static PseudoClass errorClass = PseudoClass.getPseudoClass("error");
+
+    @FXML
+    private Button run;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private ChoiceBox transtype;
+    @FXML
+    private TextField input;
+    @FXML
+    private TextField output;
+
+    @Override
+    public void initialize(final URL url, final ResourceBundle rb) {
+        final List<String> list = getTranstypes();
+        final ObservableList obList = observableList(list);
+        transtype.getItems().clear();
+        transtype.setItems(obList);
+        transtype.valueProperty().addListener((observable, oldValue, newValue) -> {
+            validate();
+        });
+
+        input.textProperty().addListener((observable, oldValue, newValue) -> {
+            final boolean v = !new File(newValue).exists();
+            input.pseudoClassStateChanged(errorClass, v);
+            validate();
+        });
+
+        output.textProperty().addListener((observable, oldValue, newValue) -> {
+            final boolean v = !new File(newValue).exists();
+            output.pseudoClassStateChanged(errorClass, v);
+            validate();
+        });
+    }
+
+    private List<String> getTranstypes() {
+        final Document plugins;
+        try (final InputStream in = getClass().getResourceAsStream("/plugins.xml")) {
+            plugins = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
+
+            final List<String> res = new ArrayList<>();
+            final NodeList transtypes = plugins.getElementsByTagName("transtype");
+            for (int i = 0; i < transtypes.getLength(); i++) {
+                final Element transtype = (Element) transtypes.item(i);
+                res.add(transtype.getAttribute("name"));
+            }
+            Collections.sort(res);
+
+            return res;
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            throw new RuntimeException("Failed to read plugin configuration: " + e.getMessage(), e);
+        }
+    }
+
+    private void validate() {
+        boolean valid = true;
+        if (valid) {
+            valid = !input.getText().isEmpty();
+        }
+        if (valid) {
+            valid = transtype.getValue() != null;
+        }
+        if (valid) {
+            valid = !output.getText().isEmpty();
+        }
+
+        run.setDisable(!valid);
+    }
+
+    @FXML
+    protected void run(ActionEvent event) {
+        System.err.println("run " + transtype.getValue() + " for " + input.getText());
+        progressBar.setProgress(progressBar.getProgress() + 0.1d);
+    }
+
+    @FXML
+    protected void changeInput(ActionEvent event) {
+        System.out.println("change");
+    }
+
+    @FXML
+    protected void findInput(ActionEvent event) {
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Input file");
+        fileChooser.setSelectedExtensionFilter(new ExtensionFilter("DITA map", "ditamap"));
+        final Window window = ((Node) event.getTarget()).getScene().getWindow();
+        final File in = fileChooser.showOpenDialog(window);
+        input.setText(in != null ? in.getAbsolutePath() : null);
+    }
+
+    @FXML
+    protected void findOutput(ActionEvent event) {
+        final DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setTitle("Output directory");
+        final Window window = ((Node) event.getTarget()).getScene().getWindow();
+        final File out = dirChooser.showDialog(window);
+        output.setText(out != null ? out.getAbsolutePath() : null);
+    }
+}
